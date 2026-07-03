b6755211b2eb37807be50964f0cdf664dede5bfd
Nathan-Matharu <nathanmatharu@gmail.com>
2026-07-02 20:06:02 -0700

    Update site URL to custom domain matharu.work in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Nathan-Matharu.github.io',
+  site: 'https://matharu.work',
 });
