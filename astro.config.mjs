b7a373943194a15f8916ae3202762b28f49708f1
Nathan-Matharu <nathanmatharu@gmail.com>
2026-07-08 22:34:00 -0700

    Added Boomerang publication video, migrated to mdx, optimized images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://matharu.work',
+  integrations: [mdx()],
 });
